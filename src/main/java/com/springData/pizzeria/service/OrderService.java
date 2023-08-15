a6a5619e4c6fe03c9f5be4ff5af339996fea183a
elyeyoflex <elyeyoflex@gmail.com>
2023-08-15 12:32:33 -0300

    creo rest para order, y realizo el uso de lazy e eager

---
+++ src/main/java/com/springData/pizzeria/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.springData.pizzeria.service;
+
+import com.springData.pizzeria.persistence.entity.OrderEntity;
+import com.springData.pizzeria.persistence.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<OrderEntity> getAll(){
+        return this.orderRepository.findAll();
+    }
+}
