d45cd4d7fda614b8881ae4e880144ba5c8897f9a
elyeyoflex <elyeyoflex@gmail.com>
2023-08-23 10:09:40 -0300

    usar @Query SQL nativos

--- src/main/java/com/springData/pizzeria/persitence/repository/OrderRepository.java
+++ src/main/java/com/springData/pizzeria/persitence/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.springData.pizzeria.persitence.repository;
 
 import com.springData.pizzeria.persitence.entity.OrderEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
     List<OrderEntity> findAllByDateAfter(LocalDateTime date);
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+    @Query(value = "select * from pizza_order where id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
+
+
 }
